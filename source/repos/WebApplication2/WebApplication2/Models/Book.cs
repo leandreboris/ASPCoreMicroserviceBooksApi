4fff6cf6bb1d4419bc461da421b29cba4de7dae7
leandreboris <64496474+leandreboris@users.noreply.github.com>
2021-09-07 13:59:48 +0100

    First commit

---
+++ source/repos/WebApplication2/WebApplication2/Models/Book.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication2.Models
+{
+    public class Book
+
+    {
+        public long Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+
+
+    }
+}
